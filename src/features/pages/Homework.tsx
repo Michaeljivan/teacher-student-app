acd997e2da5e49ae9c0ef48501b00dbb61478412
Michael Jivan <mjivan7@gmail.com>
2026-05-28 09:49:39 -0500

    add tsx pages

---
+++ src/features/pages/Homework.tsx
@@ -0,0 +1,9 @@
+const Homework: React.FC = () => {
+    return (
+        <div>
+            <h1>Homework Page</h1>
+        </div>
+    );
+};
+
+export default Homework;
---
+++ src/features/pages/TeacherAdmin.tsx
@@ -0,0 +1,9 @@
+const TeacherAdmin: React.FC = () => {
+    return (
+        <div>
+            <h1>Teacher Admin Page</h1>
+        </div>
+    );
+};
+
+export default TeacherAdmin;
